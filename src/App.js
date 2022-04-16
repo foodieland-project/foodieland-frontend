4615630f0cb099ee39a562b66068b8bcbc13c7f7
shakiba Hamzeh <shakiba.h_70@yahoo.com>
2022-04-16 22:53:08 +0430

    i made moreRecipe component and styled it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Recipe from "./components/BasicComponents/recipe";
 import BannerCard from "./components/BasicComponents/recipe/components/bannerCard";
+import MoreRecipe from './components/BasicComponents/recipe/components/MoreRecipe';
 
 function App() {
   return (
     <div className="App">
-      <Recipe />
+     {/*<Recipe />*/  }
+
+     <MoreRecipe/>
+
     </div>
   )
 }
