339a6fdf5167067b9b5dca72e14fa405078e0d4d
tohid <tohidebrahimi17@gmail.com>
2022-04-17 15:55:34 +0430

    fixed responsive bugs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-// <<<<<<< HEAD
-// import React from "react";
 import React from "react";
 import Carousel from "./components/BasicComponents/Carousel/Carousel";
 import Recipe from "./components/BasicComponents/recipe";
+
 import Cooker from "./components/BasicComponents/cooker";
+import MoreRecipe from "./components/BasicComponents/recipe/components/MoreRecipe";
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
       <Carousel />
       <Recipe />
       <Cooker />
+      <MoreRecipe />
     </div>
   );
 }
--- src/components/BasicComponents/cooker/index.tsx
+++ src/components/BasicComponents/cooker/index.tsx
@@ -9,17 +9,17 @@ function Cooker() {
 
     return (
         <div className='flex lg:justify-around xl:flex-row 2sm:flex-col md:justify-center 2sm:justify-center md:items-center'>
-            <div className="flex flex-col 2sm:mb-40 justify-center items-center" >
-                <p className="font-inter font-semibold text-5xl w-auto md:w-[526px]  tracking-tighter mb-[24px] text-center xl:text-left" >Everyone can be a chef in their own kitchen</p>
+            <div className="flex flex-col mb-24 md:mb-40 justify-center items-center" >
+                <p className="font-inter font-semibold text-4xl md:text-5xl w-auto xl:w-[526px]  tracking-tighter mb-[24px] text-center xl:text-left" >Everyone can be a chef in their own kitchen</p>
                 <p className="font-inter font-normal text-base w-auto md:w-[526px] mb-[72px] text-center xl:text-left" >Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim </p>
-                <button className="flex justify-center items-center w-[180px] h-[60px] bg-black text-white rounded-2xl" >Learn More</button>
+                <button className="flex justify-center items-center w-36 md:w-[180px] h-12 md:h-[60px] bg-black text-white rounded-2xl" >Learn More</button>
             </div>
             <div className='w-auto md:w-[651px] h-[500px] bg-gradient-to-b from-white to-blue-50 relative rounded-3xl'>
-                <img className="absolute object-contain bottom-0 right-20 h-[597px] W-[660px]" src="images/cooker/image-220.png" alt="" />
-                <img className="absolute top-80 right-[626px]" src="images/cooker/image-27.png" alt="" />
-                <img className="absolute top-[70px] right-[36px]" src="images/cooker/image-20.png" alt="" />
-                <img className="absolute right-[510px]" src="images/cooker/image-21.png" alt="" />
-                <img className="absolute  right-[180px]" src="images/cooker/image-28.png" alt="" />
+                <img className="absolute object-contain bottom-0  h-[597px] W-[660px]" src="images/cooker/image-220.png" alt="" />
+                <img className="absolute w-10 sm:w-12 top-80 md:right-[626px]" src="images/cooker/image-27.png" alt="" />
+                <img className="absolute w-16 sm:w-20 top-[70px] right-[36px]" src="images/cooker/image-20.png" alt="" />
+                <img className="absolute w-20 sm:w-24 left-10 md:right-[510px]" src="images/cooker/image-21.png" alt="" />
+                <img className="absolute w-11 md:w-16 right-28 sm:right-36 md:right-[180px]" src="images/cooker/image-28.png" alt="" />
             </div>
         </div>
     )
