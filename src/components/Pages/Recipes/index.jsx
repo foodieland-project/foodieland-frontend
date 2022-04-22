a3508fae180281f419fa2b21f9a945629d13805d
tohid <tohidebrahimi17@gmail.com>
2022-04-22 15:55:16 +0430

    added subscribe card

--- src/components/Pages/Recipes/index.jsx
+++ src/components/Pages/Recipes/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import RecommendedRecipes from "../../BasicComponents/recommendedRecipes";
+import SubscribeCard from "../../BasicComponents/subscribeCard";
 
 const Recipes = () => {
   return (
     <div>
+      <SubscribeCard />
       <RecommendedRecipes />
     </div>
   );
