093565e9036bb20cfc2cb9064f7274fdcd28b190
tohid <tohidebrahimi17@gmail.com>
2022-04-23 11:59:24 +0430

    responsive done

--- src/components/BasicComponents/directions/directionCard/index.jsx
+++ src/components/BasicComponents/directions/directionCard/index.jsx
@@ -4,7 +4,7 @@ function DirectionCard({ title, description, number, imageUrl, imageDesc }) {
   const [isChecked, setChecked] = useState(false);
 
   return (
-    <div className="flex gap-6 border-b border-gray-300 border-solid mb-10 pb-10">
+    <div className="flex gap-2 md:gap-6 border-b border-gray-300 border-solid mb-10 pb-16">
       <div className="relative pt-1.5">
         <input
           type="checkbox"
@@ -16,20 +16,24 @@ function DirectionCard({ title, description, number, imageUrl, imageDesc }) {
           <span
             className={`${
               isChecked ? "custom-checkbox" : ""
-            } w-6 h-6 rounded-[50%] border-2 border-solid border-black `}
+            } w-5 h-5 md:w-6 md:h-6 rounded-[50%] border-2 border-solid border-black `}
           ></span>
         </label>
       </div>
       <div>
-        <h2 className="font-semibold text-2xl mb-5  ">
+        <h2 className="font-semibold text-xl md:text-2xl mb-5  ">
           <span>{number + 1}.</span>
           <span>{title}</span>
         </h2>
-        <p className="text-secondary">{description}</p>
+        <p className="text-secondary text-justify text-sm md:text-base">
+          {description}
+        </p>
         {imageUrl && (
           <div className="mt-10 ">
             <img src={`${imageUrl}`} alt="" />
-            <p className="text-secondary mt-10">{imageDesc}</p>
+            <p className="text-secondary mt-10 text-justify text-sm md:text-base">
+              {imageDesc}
+            </p>
           </div>
         )}
       </div>
--- src/components/BasicComponents/directions/index.jsx
+++ src/components/BasicComponents/directions/index.jsx
@@ -31,23 +31,25 @@ function Directions() {
     },
   ];
   return (
-    <section className="w-2/3 font-inter">
-      <div className="my-12">
-        <h2 className="font-semibold text-4xl">Directions</h2>
-      </div>
-      <div>
-        {directionsData.map(
-          ({ id, title, description, imageUrl, imageDesc }, index) => (
-            <DirectionCard
-              key={id}
-              title={title}
-              description={description}
-              number={index}
-              imageUrl={imageUrl}
-              imageDesc={imageDesc}
-            />
-          )
-        )}
+    <section className="my-16 md:my-24 flex w-11/12 mx-auto xl:w-full font-inter">
+      <div className="basis-[100%] lg:basis-[66%]">
+        <div className="my-12">
+          <h2 className="font-semibold text-3xl md:text-4xl">Directions</h2>
+        </div>
+        <div>
+          {directionsData.map(
+            ({ id, title, description, imageUrl, imageDesc }, index) => (
+              <DirectionCard
+                key={id}
+                title={title}
+                description={description}
+                number={index}
+                imageUrl={imageUrl}
+                imageDesc={imageDesc}
+              />
+            )
+          )}
+        </div>
       </div>
     </section>
   );
