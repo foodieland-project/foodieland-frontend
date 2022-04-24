362e61ec2955398ca8a4713eec903a979b41175f
Nima Nekouei <nekoueinima@protonmail.com>
2022-04-24 22:06:37 +0430

    cook image added and layout created

--- src/components/BasicComponents/contact/index.jsx
+++ src/components/BasicComponents/contact/index.jsx
@@ -1,6 +1,8 @@
 import Input from './ContactInput/Input';
 import Select from './ContactInput/Select';
 import TextArea from './ContactInput/TextArea';
+
+
 const options = [
     {id: "Advertising",
     name: "Advertising"},
@@ -13,10 +15,12 @@ const options = [
 const Contact = (props) => {
     return ( 
         <>
-        <h2 className="" >Contact us</h2>
-        <div>
-            <img src="../../../../public/images/Cook-contact.png" alt="" />
-            <form className='grid grid-cols-2 grid-rows-4 gap-x-10 '>
+        <div className='grid grid-cols-3 w-full my-20' >
+            <h2 className="text-6xl font-semibold tracking-tight col-start-2" >Contact us</h2>
+        </div>
+        <div className="grid grid-cols-3 mb-44" >
+        <img src="http://localhost:3000/images/cooker/Cook-contact.png" alt="Cook" className='h-5/6'/>
+            <form className='grid grid-cols-2 grid-rows-4 gap-x-10 col-span-2  '>
                 <Input id="name" label="NAME" type="text" placeholder="Enter your name..." />
                 <Input id="email" label="EMAIL ADDRESS" type="text" placeholder="Enter your email..." />
                 <Input id="subject" label="SUBJECT" type="text" placeholder="Enter subject..." />
