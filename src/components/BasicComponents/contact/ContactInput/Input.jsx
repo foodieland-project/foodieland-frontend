a6008459b8e32aad60ab12324502ecc51dcaa394
Nima Nekouei <nekoueinima@protonmail.com>
2022-04-26 00:45:11 +0430

    input component validation added

--- src/components/BasicComponents/contact/ContactInput/Input.jsx
+++ src/components/BasicComponents/contact/ContactInput/Input.jsx
@@ -1,11 +1,65 @@
-const Input = (props) => {
+import { useState, useEffect } from "react";
 
-    return ( 
-        <div className="flex flex-col font-inter ">
-            <label className="text-xs font-medium mb-3 tracking-wide opacity-80" htmlFor={props.id}>{props.label}</label>
-            <input className="border border-gray-900/10 p-5 rounded-2xl text-sm mb-6 text-black/60" type={props.type} id={props.id} placeholder={props.placeholder}/>
-        </div>
-     );
-}
- 
+const isNotEmpty = (value) => value.trim() !== "";
+const isEmail = (value) => value.includes("@");
+
+const Input = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
+  const [isValid, setIsValid] = useState(false);
+
+  const valueChangeHandler = (event) => {
+    setEnteredValue(event.target.value);
+  };
+
+  const valueBlurHandler = () => {
+    setIsTouched(true);
+  };
+
+  const hasError = !isValid && isTouched;
+
+  useEffect(() => {
+    if (props.id === "email") {
+      setIsValid(isEmail(enteredValue));
+      props.onChange({
+        id: props.id,
+        value: enteredValue,
+        isValid: hasError,
+      });
+    } else {
+      setIsValid(isNotEmpty(enteredValue));
+      props.onChange({
+        id: props.id,
+        value: enteredValue,
+        isValid: hasError,
+      });
+    }
+  }, [enteredValue, hasError]);
+
+  const inputClasses = `border p-5 rounded-2xl text-sm mb-6 text-black/60 ${
+    hasError
+      ? "border-red-600 bg-red-300/10"
+      : "border-gray-900/10 bg-transparen"
+  }`;
+
+  return (
+    <div className="flex flex-col font-inter ">
+      <label
+        className="text-xs font-medium mb-3 tracking-wide opacity-80"
+        htmlFor={props.id}
+      >
+        {props.label}
+      </label>
+      <input
+        onChange={valueChangeHandler}
+        onBlur={valueBlurHandler}
+        className={inputClasses}
+        type={props.type}
+        id={props.id}
+        placeholder={props.placeholder}
+      />
+    </div>
+  );
+};
+
 export default Input;
--- src/components/BasicComponents/contact/index.jsx
+++ src/components/BasicComponents/contact/index.jsx
@@ -1,36 +1,80 @@
-import Input from './ContactInput/Input';
-import Select from './ContactInput/Select';
-import TextArea from './ContactInput/TextArea';
+import { useState } from "react";
 
+import Input from "./ContactInput/Input";
+import Select from "./ContactInput/Select";
+import TextArea from "./ContactInput/TextArea";
 
 const options = [
-    {id: "Advertising",
-    name: "Advertising"},
-    {id: "Commercials",
-    name: "Commercial"},
-    {id: "Support",
-    name: "Support"},
-]
+  { id: "Advertising", name: "Advertising" },
+  { id: "Commercials", name: "Commercial" },
+  { id: "Support", name: "Support" },
+];
 
 const Contact = (props) => {
-    return ( 
-        <>
-        <div className='flex justify-center lg:grid lg:grid-cols-3 w-full my-20' >
-            <h2 className="text-6xl font-semibold tracking-tight col-start-2" >Contact us</h2>
-        </div>
-        <div className="flex flex-col lg:grid lg:grid-cols-3 mb-44" >
-        <img src="http://localhost:3000/images/cooker/Cook-contact.png" alt="Cook" className='mb-16 justify-self-center self-center lg:h-5/6 ml-3'/>
-            <form className=' flex flex-col px-10 sm:px-32 lg:grid lg:grid-cols-2 lg:grid-rows-4 gap-x-10 lg:col-span-2 lg:px-5  '>
-                <Input id="name" label="NAME" type="text" placeholder="Enter your name..." />
-                <Input id="email" label="EMAIL ADDRESS" type="text" placeholder="Enter your email..." />
-                <Input id="subject" label="SUBJECT" type="text" placeholder="Enter subject..." />
-                <Select id="type" label="ENQUIRY TYPE" options={options} />
-                <TextArea id="message" label="MESSAGES" placeholder="Enter your Messages..." />
-                <button className="flex justify-center items-center w-36 md:w-[180px] h-12 md:h-[60px] bg-black text-white rounded-2xl font-semibold " >Submit</button>
-            </form>
-        </div>
-        </>
-     );
-}
- 
+  const [inputValue, setInputValue] = useState({});
+
+  const inputChangeHandler = (data) => {
+    setInputValue((prevState) => {
+      return {
+        ...prevState,
+        [data.id]: {
+          value: data.value,
+          isValid: data.isValid,
+        },
+      };
+    });
+  };
+
+  console.log(inputValue);
+
+  return (
+    <>
+      <div className="flex justify-center lg:grid lg:grid-cols-3 w-full my-20">
+        <h2 className="text-6xl font-semibold tracking-tight col-start-2">
+          Contact us
+        </h2>
+      </div>
+      <div className="flex flex-col lg:grid lg:grid-cols-3 mb-44">
+        <img
+          src="http://localhost:3000/images/cooker/Cook-contact.png"
+          alt="Cook"
+          className="mb-16 justify-self-center self-center lg:h-5/6 ml-3"
+        />
+        <form className=" flex flex-col px-10 sm:px-32 lg:grid lg:grid-cols-2 lg:grid-rows-4 gap-x-10 lg:col-span-2 lg:px-5  ">
+          <Input
+            onChange={inputChangeHandler}
+            id="name"
+            label="NAME"
+            type="text"
+            placeholder="Enter your name..."
+          />
+          <Input
+            onChange={inputChangeHandler}
+            id="email"
+            label="EMAIL ADDRESS"
+            type="text"
+            placeholder="Enter your email..."
+          />
+          <Input
+            onChange={inputChangeHandler}
+            id="subject"
+            label="SUBJECT"
+            type="text"
+            placeholder="Enter subject..."
+          />
+          <Select id="type" label="ENQUIRY TYPE" options={options} />
+          <TextArea
+            id="message"
+            label="MESSAGES"
+            placeholder="Enter your Messages..."
+          />
+          <button className="flex justify-center items-center w-36 md:w-[180px] h-12 md:h-[60px] bg-black text-white rounded-2xl font-semibold ">
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
+
 export default Contact;
