c1f8fc4b8eaaf98941311415c1f7c0f715cd24e7
alireza babaie <60476541+babaie774@users.noreply.github.com>
2022-04-26 16:49:49 +0430

    fix-aboutus-bugs

--- src/components/Pages/aboutUs/index.jsx
+++ src/components/Pages/aboutUs/index.jsx
@@ -1,9 +1,6 @@
 import SubscribeCard from "../../BasicComponents/subscribeCard";
 import React from "react";
-import { icons } from "../../../utils/icons";
-import RecipeVideoCard from "../../BasicComponents/recipeVideoCard";
 import RecommendedRecipes from "../../BasicComponents/recommendedRecipes";
-import Galleries from "../../BasicComponents/galleries";
 import Chefs from "../../BasicComponents/chefs";
 import AboutUsInfo from "../../BasicComponents/aboutUsInfo/index.";
 
