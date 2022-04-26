5b61f2e2648be05ab20614aa4dfab70d5773e510
tohid ebrahimi <tohidebrahimi17@gmail.com>
2022-04-26 19:54:16 +0430

    added dynamic behaviour

--- src/components/BasicComponents/recommendedRecipes/index.jsx
+++ src/components/BasicComponents/recommendedRecipes/index.jsx
@@ -1,5 +1,5 @@
 import MoreRecipeCard from "../moreRecipe/components/moreRecipeCard";
-function RecommendedRecipes() {
+function RecommendedRecipes({title,length}) {
   const recommendedRecipesData = [
     {
       category: "product",
@@ -42,11 +42,11 @@ function RecommendedRecipes() {
     <section className="container font-inter flex flex-col py-2  mx-auto my-10 md:my-20">
       <div className="text-center mb-16">
         <h2 className="font-semibold text-3xl sm:text-4xl">
-          You may like these recipes too
+          {title}
         </h2>
       </div>
       <div className="flex gap-6 justify-center items-center w-full flex-wrap">
-        {recommendedRecipesData.slice(0, 4).map((data) => (
+        {recommendedRecipesData.slice(0, length).map((data) => (
           <MoreRecipeCard key={data.id} recipeData={data} />
         ))}
       </div>
