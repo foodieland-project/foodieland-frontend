e21de26a65040e7afbb3fe02e31adabf066109bb
shakiba Hamzeh <shakiba.h_70@yahoo.com>
2022-04-29 23:09:40 +0430

    start statistic component

--- src/components/Pages/panel/statistic/index.jsx
+++ src/components/Pages/panel/statistic/index.jsx
@@ -1,12 +1,87 @@
 import React from "react";
+
 import PanelLayout from "../../../layout/panelLayout";
+import Chart from "../../../BasicComponents/statistic/components/chart/index.jsx";
+const userData = [
+  {
+    "name": "Jan",
+    "uv": 4000,
+    "pv": 2400,
+    "amt": 2400
+  },
+  {
+    "name": "Feb",
+    "uv": 3000,
+    "pv": 1398,
+    "amt": 2210
+  },
+  {
+    "name": "Mar",
+    "uv": 2000,
+    "pv": 9800,
+    "amt": 2290
+  },
+  {
+    "name": "Apr",
+    "uv": 2780,
+    "pv": 3908,
+    "amt": 2000
+  },
+  {
+    "name": "May",
+    "uv": 1890,
+    "pv": 4800,
+    "amt": 2181
+  },
+  {
+    "name": "Jun",
+    "uv": 2390,
+    "pv": 3800,
+    "amt": 2500
+  },
+  {
+    "name": "Jul",
+    "uv": 3490,
+    "pv": 4300,
+    "amt": 2100
+  },
+  {
+    "name": "Aug",
+    "uv": 3490,
+    "pv": 4300,
+    "amt": 2100
+  },
+  {
+    "name": "Sep",
+    "uv": 3490,
+    "pv": 4300,
+    "amt": 2100
+  },
+  {
+    "name": "Oct",
+    "uv": 3490,
+    "pv": 4300,
+    "amt": 2100
+  },
+  {
+    "name": "Nov",
+    "uv": 3490,
+    "pv": 4300,
+    "amt": 2100
+  },
+  {
+    "name": "Dec",
+    "uv": 3490,
+    "pv": 4300,
+    "amt": 2100
+  }
+]
+
 
 const Statistic = () => {
   return (
     <PanelLayout>
-      <p>
-        Statistic
-      </p>
+      <Chart data={userData} title="Monthly Analytics"/>
     </PanelLayout>
   );
 };
