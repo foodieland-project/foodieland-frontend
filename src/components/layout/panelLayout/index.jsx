d40c19a158af80a3fb46e3f128b61d0ce4d4e050
tohid <tohidebrahimi17@gmail.com>
2022-04-30 15:49:44 +0430

    fixed some naming bugs

--- src/components/layout/panelLayout/index.jsx
+++ src/components/layout/panelLayout/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PanelFooter from "./panelFooter";
 import PanelHeader from "./panelHeader";
-import PanelSidebar from "./panelSideBar";
+import PanelSidebar from "./panelSidebar";
 
 const PanelLayout = ({ children }) => {
   return (
