51908a670bed9c97b7a6d7b7ed4ae4d92503605a
Nima Nekouei <nekoueinima@protonmail.com>
2022-05-02 15:41:43 +0430

    hover and active effect added

--- src/components/layout/panelLayout/panelOptionNavbar/index.jsx
+++ src/components/layout/panelLayout/panelOptionNavbar/index.jsx
@@ -1,20 +1,23 @@
 import { icons } from "../../../pages/user/utils/icons";
+import styles from "./index.module.css";
+
+const listClasses = `${styles["nav__links"]} mx-8 cursor-pointer flex items-center`;
 
 const PanelNav = (props) => {
   return (
     <main className="w-5/6 mx-5 my-3 h-5/6 bg-white shadow-xl ">
-      <nav>
-        <ul className="flex justify-start py-3 border-b-2 border-slate-300">
-          <li className="mx-8 cursor-pointer flex items-center">
-            <span className="mr-2 fill-slate-800 ">{icons.search()}</span>{" "}
+      <nav className="border-b-2 border-slate-300 py-3">
+        <ul className="flex justify-start">
+          <li className={listClasses}>
+            <span className="mr-2 fill-slate-800">{icons.search()}</span>{" "}
             <p>ACCOUNT</p>
           </li>
-          <li className="mx-8 cursor-pointer flex items-center">
-            <span className="mr-2 fill-slate-800 ">{icons.security()}</span>{" "}
+          <li className={listClasses}>
+            <span className="mr-2 fill-slate-800">{icons.security()}</span>{" "}
             <p>SECURITY</p>
           </li>
-          <li className="mx-8 cursor-pointer flex items-center">
-            <span className="mr-2 fill-slate-800 ">{icons.info()}</span>{" "}
+          <li className={listClasses}>
+            <span className="mr-2 fill-slate-800">{icons.info()}</span>{" "}
             <p>INFO</p>
           </li>
         </ul>
