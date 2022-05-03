505bcdde18473b8c3223473d3050a0ee25c6a1b0
tohid <tohidebrahimi17@gmail.com>
2022-05-03 15:53:18 +0430

    added panel layout finished

--- src/components/layout/panelLayout/panelSidebar/optionCard/index.jsx
+++ src/components/layout/panelLayout/panelSidebar/optionCard/index.jsx
@@ -1,20 +1,20 @@
-import {
-  Link
-} from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { icons } from "../../../../../utils/icons";
 
 function OptionCard({ id, option, icon, selected, selectHandler, route }) {
+  const { pathname } = useLocation();
   return (
     <>
       <Link rel="stylesheet" to={`${route}`}>
         <div
-          className={`${selected && "active"
-            } option-selected flex items-center py-3 text-lightBlack text-sm lg:text-base font-inter pl-4 lg:pl-6 cursor-pointer`}
+          className={`${
+            pathname === route && "active"
+          } option-selected flex items-center py-3 text-lightBlack text-sm lg:text-base font-inter pl-4 lg:pl-6 cursor-pointer`}
         >
           <span>{icons[icon]()}</span>
           <span className="ml-3 capitalize">{option}</span>
-        </div >
-      </Link >
+        </div>
+      </Link>
     </>
   );
 }
