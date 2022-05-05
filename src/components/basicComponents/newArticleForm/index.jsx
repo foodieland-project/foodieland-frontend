4f8e64ec0054d3a08e3a7e6a53781c6039a74cf3
tohid <tohidebrahimi17@gmail.com>
2022-05-05 20:54:32 +0430

    new articleui done

--- src/components/basicComponents/newArticleForm/index.jsx
+++ src/components/basicComponents/newArticleForm/index.jsx
@@ -1,5 +1,49 @@
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 function NewArticleForm() {
-  return <div>new article form</div>;
+  return (
+    <div className="p-4">
+      <form action="">
+        <div className="flex flex-col">
+          <label htmlFor="title" className="pb-2 pl-2">
+            Title
+          </label>
+          <input
+            type="text"
+            name="title"
+            className={` md:w-[60%] lg:w-[40%] border border-gray-500  rounded-2xl py-2 px-6 `}
+          />
+        </div>
+        <div className="flex flex-col mt-4">
+          <label htmlFor="" className="pb-2 pl-2">
+            Description
+          </label>
+          <CKEditor editor={ClassicEditor} data="" />
+        </div>
+        <div className="flex flex-col mt-4">
+          <label htmlFor="" className="pb-2 pl-2">
+            Upload Cooking image
+          </label>
+          <input type="file" className="pl-2" />
+        </div>
+        <div className="flex flex-col mt-4">
+          <label htmlFor="" className="pb-2 pl-2">
+            Upload date of publication
+          </label>
+          <input type="date" className="pl-2" />
+        </div>
+
+        <div className="mt-4 flex justify-end">
+          <button
+            type="submit"
+            className="bg-darkPurple text-white py-2 px-6 rounded-xl cursor-pointer mr-2 w-full sm:w-auto"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default NewArticleForm;
