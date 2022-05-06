ad97b144a17c8d6480527e4e1ee14414f6dcbe21
alireza babaie <60476541+babaie774@users.noreply.github.com>
2022-05-07 04:23:20 +0430

    init-file-routes

---
+++ src/utils/routes.js
@@ -0,0 +1,34 @@
+//user routes
+import AboutUs from "../components/pages/user/aboutUs";
+import Blog from "../components/pages/user/blog";
+import Contact from "../components/pages/user/contact";
+import Home from "../components/pages/user/home";
+import Recipes from "../components/pages/user/recipes";
+import NotFound from "../components/pages/user/notFound";
+import SingleBlog from "../components/pages/user/singleBlog";
+//panel routes
+import Login from "../components/pages/panel/login";
+import NewPost from "../components/pages/panel/newPost";
+import Notice from "../components/pages/panel/notice";
+import Option from "../components/pages/panel/option";
+import Posts from "../components/pages/panel/posts";
+import Statistic from "../components/pages/panel/statistic";
+import Contacts from "../components/pages/panel/contacts";
+
+
+export {
+    AboutUs,
+    Blog,
+    Contact,
+    Home,
+    Recipes,
+    NotFound,
+    SingleBlog,
+    Login,
+    NewPost,
+    Notice,
+    Option,
+    Posts,
+    Statistic,
+    Contacts
+}
