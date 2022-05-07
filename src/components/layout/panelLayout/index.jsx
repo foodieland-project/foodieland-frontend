4879e056752334faa61fb38242a39c587ff95f90
tohid <tohidebrahimi17@gmail.com>
2022-05-07 21:05:12 +0430

    update panel layout

--- src/components/layout/panelLayout/index.jsx
+++ src/components/layout/panelLayout/index.jsx
@@ -12,7 +12,7 @@ const PanelLayout = ({ children }) => {
       <div className="w-[97%] sm:w-4/5 md:w-[80%] mx-auto mt-7 md:mt-0 md:mb-0 md:absolute right-0 bg-lightPurple">
         <div className="relative min-h-screen">
           <PanelHeader />
-          <div className="flex  justify-center pt-2 pb-16">
+          <div className="flex  justify-center pb-14">
             <main className="w-full md:w-11/12 mx-5  my-3 h-5/6 bg-white shadow-xl rounded-md">
               {children}
             </main>
