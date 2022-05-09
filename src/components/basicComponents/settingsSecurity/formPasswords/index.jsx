5d114c8be8eaaee5d03f6040524ce18f28454282
tohid <tohidebrahimi17@gmail.com>
2022-05-09 10:23:30 +0430

    added show password feature to form passwords

--- src/components/basicComponents/settingsSecurity/formPasswords/index.jsx
+++ src/components/basicComponents/settingsSecurity/formPasswords/index.jsx
@@ -1,45 +1,63 @@
+import { useState } from "react";
 import { icons } from "../../../../utils/icons";
 
 function FormPasswords() {
-  function showPasswordHandler() {}
+  const [currentInputType, setCurrentInputType] = useState("password");
+  const [newInputType, setNewInputType] = useState("password");
+  const [confirmInputType, setConfirmInputType] = useState("password");
+  function showPasswordHandler(type) {
+    if (type === "current") {
+      setCurrentInputType(
+        currentInputType === "password" ? "text" : "password"
+      );
+    }
+    if (type === "new") {
+      setNewInputType(newInputType === "password" ? "text" : "password");
+    }
+    if (type === "confirm") {
+      setConfirmInputType(
+        confirmInputType === "password" ? "text" : "password"
+      );
+    }
+  }
   return (
     <div className="flex border-b border-gray-200 pb-4 lg:pb-0">
       <div className="basis-full lg:basis-1/2">
         <div className="relative">
           <input
-            type="text"
+            type={currentInputType}
             placeholder="Current Password"
             className="border-2 border-gray-300 rounded-md py-2.5 w-full outline-none pl-2 pr-6"
           />
           <span
-            className="absolute right-3 top-4"
-            onClick={showPasswordHandler}
+            className="absolute right-3 top-4 cursor-pointer"
+            onClick={() => showPasswordHandler("current")}
           >
             {icons.eye()}
           </span>
         </div>
         <div className="relative mt-4">
           <input
-            type="text"
+            type={newInputType}
             placeholder="New Password"
             className="border-2 border-gray-300 rounded-md py-2.5 w-full outline-none pl-2 pr-6"
           />
           <span
-            className="absolute right-3 top-4"
-            onClick={showPasswordHandler}
+            className="absolute right-3 top-4 cursor-pointer"
+            onClick={() => showPasswordHandler("new")}
           >
             {icons.eye()}
           </span>
         </div>
         <div className="relative mt-4">
           <input
-            type="text"
+            type={confirmInputType}
             placeholder="Confirm New Password"
             className="border-2 border-gray-300 rounded-md py-2.5 w-full outline-none pl-2 pr-6"
           />
           <span
-            className="absolute right-3 top-4"
-            onClick={showPasswordHandler}
+            className="absolute right-3 top-4 cursor-pointer"
+            onClick={() => showPasswordHandler("confirm")}
           >
             {icons.eye()}
           </span>
