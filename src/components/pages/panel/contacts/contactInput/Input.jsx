c64ba38a41e4f9288076fbb4b40fa32fd651d419
Siavash <siavash.khani200@gmail.com>
2022-05-10 18:26:42 +0430

    working on contact inputs

---
+++ src/components/pages/panel/contacts/contactInput/Input.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Input = (props) => (
+  <div className="flex flex-col">
+    <label
+      className="text-md font-medium my-3 tracking-wide"
+      htmlFor={props.type}
+    >
+      {props.label}
+    </label>
+    <input
+      className="w-full border-2 rounded-md p-3 mb-4 outline-none"
+      type={props.type}
+      placeholder={props.placeholder}
+      /* onChange={onChange} */
+      value={props.value}
+      id={props.id}
+      name={props.name}
+    />
+  </div>
+);
+
+export default Input;
--- src/components/pages/panel/contacts/index.jsx
+++ src/components/pages/panel/contacts/index.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import PanelLayout from "../../../layout/panelLayout";
+import Input from "./contactInput/Input";
 
 const Contacts = () => {
-  return <PanelLayout></PanelLayout>;
+  return (
+    <PanelLayout>
+      <form className="flex flex-col px-10 sm:px-32 lg:grid lg:grid-cols-2 lg:grid-rows-4 gap-x-10 lg:col-span-2 lg:px-5 ">
+        <div className="">
+          <Input
+            label="Name"
+            placeholder="Enter your name"
+            type="text"
+            id="name"
+          />
+
+          <Input
+            label="Email"
+            placeholder="Enter your Email"
+            type="email"
+            id="email"
+          />
+        </div>
+      </form>
+    </PanelLayout>
+  );
 };
 
 export default Contacts;
