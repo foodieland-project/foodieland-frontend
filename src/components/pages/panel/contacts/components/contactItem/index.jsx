3111190ecc9771af4abbd338ca8b4137bcc6bf79
Siavash <siavash.khani200@gmail.com>
2022-05-11 15:08:15 +0430

    image removed

--- src/components/pages/panel/contacts/components/contactItem/index.jsx
+++ src/components/pages/panel/contacts/components/contactItem/index.jsx
@@ -3,7 +3,6 @@ import React from "react";
 function ContactItem({ user, subject, role, message,image }) {
   return (
     <div className="flex flex-row justify-between flex-1 border-darkPurple border-[1px] h-5 p-6 rounded-md items-center my-1">
-      <img src="/images/panel/Avatar.png" alt="avatar" className="" />
       <p className="w-[100%] sm:w-[80%] md:[50%] text-center ">{user}</p>
       <p className="sm:w-[20%] md:[10%] text-center sm:block hidden">
         {subject}
--- src/components/pages/panel/contacts/index.jsx
+++ src/components/pages/panel/contacts/index.jsx
@@ -12,8 +12,8 @@ const Contacts = () => {
       <section className="p-4 font-inter">
         <ContactTitle />
         {/* <ContactItem key="id" id="id" user="user" subject="subject" role="role" message="message"/> */}
-        {contacts.map(({ id, user, subject, role, message, image }) => (
-          <ContactItem key={id} user={user} role={role} message={message} subject={subject} image={image}/>
+        {contacts.map(({ id, user, subject, role, message }) => (
+          <ContactItem key={id} user={user} role={role} message={message} subject={subject}/>
         ))}
       </section>
     </PanelLayout>
