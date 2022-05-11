4d0841644fa966588bb0359c8a0985c9d9d90d30
Siavash <siavash.khani200@gmail.com>
2022-05-11 21:40:50 +0430

    no changes in postDashboardList <component> ...

--- src/components/pages/panel/contacts/components/contactItem/index.jsx
+++ src/components/pages/panel/contacts/components/contactItem/index.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 function ContactItem({ email, subject, message }) {
-  return (
-    <div className="flex flex-row justify-between flex-1 border-darkPurple border-[1px] mx-h-[80px] h-[80px] p-6 rounded-md items-center my-1">
-      <p className="w-[30%] sm:w-[100%] md:[50%] text-center">{email}</p>
-      <p className="sm:w-[20%] md:[10%] text-center sm:block hidden">
-        {subject}
-      </p>
+  
 
-      <p className="w-[80%] sm:w-[100%] text-center truncate pl-4">
-        {message}
-      </p>
-    </div>
+  return (
+    <>
+      <div className="flex flex-row justify-between flex-1 border-darkPurple border-[1px] mx-h-[80px] h-[80px] p-6 rounded-md items-center my-1">
+        <p className="w-[30%] sm:w-[100%] md:[50%] text-center">{email}</p>
+        <p className="sm:w-[20%] md:[10%] text-center sm:block hidden">
+          {subject}
+        </p>
+
+        <p className="w-[80%] sm:w-[100%] text-center truncate pl-4">
+          {message}
+        </p>
+      </div>
+    </>
   );
 }
 
