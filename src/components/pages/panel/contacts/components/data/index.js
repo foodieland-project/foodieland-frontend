2cc692f7266be62af4113b4c03b1229a99d564e4
Siavash <siavash.khani200@gmail.com>
2022-05-11 13:48:54 +0430

    update on data file and mapped the contactItems

--- src/components/pages/panel/contacts/components/data/index.js
+++ src/components/pages/panel/contacts/components/data/index.js
@@ -2,66 +2,66 @@ const contacts = [
     {
         id: 1,
         user: 'username',
-        subject: 'Food',
+        subject: 'Any subject',
         role: 'user',
         message: 'this is message',
-        img: "/images/panel/Avatar.png"
+        image: "/images/panel/Avatar.png"
     }, {
         id: 2,
         user: 'username',
-        subject: 'Water',
+        subject: 'Any subject',
         role: 'admin',
         message: 'this is message',
-        img: "/images/panel/Avatar.png"
+        image: "/images/panel/Avatar.png"
     }
     , {
         id: 3,
         user: 'username',
-        subject: 'Water',
+        subject: 'Any subject',
         role: 'user',
         message: 'this is message',
-        img: "/images/panel/Avatar.png"
+        image: "/images/panel/Avatar.png"
     },
     {
         id: 4,
         user: 'username',
-        subject: 'Food',
+        subject: 'Any subject',
         role: 'admin',
         message: 'this is message',
-        img: "/images/panel/Avatar.png"
+        image: "/images/panel/Avatar.png"
     },
     {
         id: 5,
         user: 'username',
-        subject: 'Food',
+        subject: 'Any subject',
         role: 'user',
         message: 'this is message',
-        img: "/images/panel/Avatar.png"
+        image: "/images/panel/Avatar.png"
     },
     {
         id: 6,
         user: 'username',
-        subject: 'Food',
+        subject: 'Any subject',
         role: 'admin',
         message: 'this is message',
-        img: "/images/panel/Avatar.png"
-    },
+        image: "/images/panel/Avatar.png"
+    }/* ,
     {
         id: 7,
         user: 'username',
-        subject: 'Recipe',
+        subject: 'Any subject',
         role: 'user',
         message: 'this is message',
-        img: "/images/panel/Avatar.png"
-    },
+        image: "/images/panel/Avatar.png"
+    } ,
     {
         id: 8,
         user: 'username',
-        subject: 'Recipe',
+        subject: 'Any subject',
         role: 'user',
         message: 'this is message',
-        img: "/images/panel/Avatar.png"
-    }
+        image: "/images/panel/Avatar.png"
+    } */
 ]
 
 export default contacts;
--- src/components/pages/panel/contacts/index.jsx
+++ src/components/pages/panel/contacts/index.jsx
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 import PanelLayout from "../../../layout/panelLayout";
 import ContactHeader from "./components/contactHeader";
 import ContactTitle from "./components/contactTitle";
-import ContactItem from './components/contactItem'
-import contacts from './components/data'
+import ContactItem from "./components/contactItem";
+import contacts from "./components/data";
 
 const Contacts = () => {
   return (
@@ -11,7 +11,10 @@ const Contacts = () => {
       <ContactHeader />
       <section className="p-4 font-inter">
         <ContactTitle />
-        <ContactItem key="id" id="id" user="user" subject="subject" role="role" message="message"/>
+        {/* <ContactItem key="id" id="id" user="user" subject="subject" role="role" message="message"/> */}
+        {contacts.map(({ id, user, subject, role, message, image }) => (
+          <ContactItem key={id} user={user} role={role} message={message} subject={subject} image={image}/>
+        ))}
       </section>
     </PanelLayout>
   );
