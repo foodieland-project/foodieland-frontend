dc4fb13b70cc6322bebeaccf99beb34cba80a0a3
Siavash <siavash.khani200@gmail.com>
2022-05-11 12:59:51 +0430

    contactTitle added

---
+++ src/components/pages/panel/contacts/components/contactTitle/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function ContactTitle({ title, category, time, view, comments }) {
+  return (
+    <div className="flex flex-row items-center text-white bg-darkPurple h-8 p-6 rounded-md my-2">
+      <p className="w-[100%] sm:w-[80%] md:[50%] text-center ">User</p>
+      <p className="sm:w-[20%] md:[10%] text-center sm:block hidden">
+        Subject
+      </p>
+      <p className="md:w-[40%] text-center md:block hidden">Role</p>
+      <p className="w-[10%] text-center xl:block hidden">View</p>
+      <p className="w-[10%] text-center xl:block hidden">Comments</p>
+    </div>
+  );
+}
+
+export default ContactTitle;
--- src/components/pages/panel/contacts/index.jsx
+++ src/components/pages/panel/contacts/index.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import PanelLayout from "../../../layout/panelLayout";
 import ContactHeader from "./components/contactHeader";
+import ContactTitle from "./components/contactTitle";
 
 const Contacts = () => {
   return (
     <PanelLayout>
       <ContactHeader />
+      <section className="p-4 font-inter">
+        <ContactTitle />
+        </section>
     </PanelLayout>
   );
 };
