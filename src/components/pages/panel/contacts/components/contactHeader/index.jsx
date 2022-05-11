ed7909c3116c88dd8f9a7f00ec73ffa2529fcee1
Siavash <siavash.khani200@gmail.com>
2022-05-11 12:43:39 +0430

    contactHeader added

---
+++ src/components/pages/panel/contacts/components/contactHeader/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ContactHeader() {
+  return (
+    <div className="text-center my-4">
+      <h2 className="text-4xl font-semibold text-[#9155fd] font-inter">
+        Contacts
+      </h2>
+    </div>
+  );
+}
+
+export default ContactHeader;
--- src/components/pages/panel/contacts/index.jsx
+++ src/components/pages/panel/contacts/index.jsx
@@ -1,16 +1,11 @@
 import React, { useState } from "react";
 import PanelLayout from "../../../layout/panelLayout";
-
+import ContactHeader from "./components/contactHeader";
 
 const Contacts = () => {
-
   return (
     <PanelLayout>
-      <div className="text-center my-4">
-        <h2 className="text-4xl font-semibold text-[#9155fd] font-inter">Contact us</h2>
-      </div>
-
-      
+      <ContactHeader />
     </PanelLayout>
   );
 };
