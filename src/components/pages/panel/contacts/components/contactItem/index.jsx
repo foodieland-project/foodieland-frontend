f64afa5ec1e8b6b6bb29b95c1d9493e89801e284
tohid <tohidebrahimi17@gmail.com>
2022-05-12 16:50:21 +0430

    fixed panel contacts message bug

--- src/components/pages/panel/contacts/components/contactItem/index.jsx
+++ src/components/pages/panel/contacts/components/contactItem/index.jsx
@@ -1,18 +1,17 @@
-import React, {useState} from "react";
-
+import React, { useState } from "react";
 
 function ContactItem({ email, subject, message }) {
-  
-
   return (
     <>
-      <div className="flex flex-row justify-between flex-1 border-darkPurple border-[1px] mx-h-[80px] h-[80px] p-6 rounded-md items-center my-1">
-        <p className="w-[30%] sm:w-[100%] md:[50%] text-center">{email}</p>
-        <p className="sm:w-[20%] md:[10%] text-center sm:block hidden">
+      <div className="flex flex-row justify-between gap-4 flex-1 border-darkPurple border-[1px] mx-h-[80px] h-[90px] rounded-md items-center my-1">
+        <p className="w-[30%] sm:w-[100%] md:w-[50%]  break-all text-center text-sm">
+          {email}
+        </p>
+        <p className="sm:w-[20%] md:w-[20%] text-center sm:block hidden font-semibold text-sm">
           {subject}
         </p>
 
-        <p className="w-[80%] sm:w-[100%] text-center truncate pl-4">
+        <p className="w-[80%] sm:w-[100%] text-center text-xs text-secondary  break-all h-full overflow-y-auto p-1">
           {message}
         </p>
       </div>
--- src/components/pages/panel/contacts/components/data/index.js
+++ src/components/pages/panel/contacts/components/data/index.js
@@ -1,63 +1,66 @@
 const contacts = [
-    {
-        id: 1,
-        email: 'foodiland@gmail.com',
-        subject: 'subject',
-        message: 'this is a message for :D tohid tohid tohid tohid tohid tohid tohid tohid tohid',
-    }, {
-        id: 2,
-        email: 'foodiland@gmail.com',
-        subject: 'subject',
-        message: 'this is a message',
-    }
-    , {
-        id: 3,
-        email: 'foodiland@gmail.com',
-        subject: 'subject',
-        message: 'this is a message',
-    },
-    {
-        id: 4,
-        email: 'foodiland@gmail.com',
-        subject: 'subject',
-        message: 'this is a message',
-    },
-    {
-        id: 5,
-        email: 'foodiland@gmail.com',
-        subject: 'subject',
-        message: 'this is a message',
-    },
-    {
-        id: 6,
-        email: 'foodiland@gmail.com',
-        subject: 'subject',
-        message: 'this is a message',
-    },
-    {
-        id: 7,
-        email: 'foodiland@gmail.com',
-        subject: 'subject',
-        message: 'this is a message',
-    },
-    {
-        id: 8,
-        email: 'foodiland@gmail.com',
-        subject: 'subject',
-        message: 'this is a message',
-    },
-    {
-        id: 9,
-        email: 'foodiland@gmail.com',
-        subject: 'subject',
-        message: 'this is a message',
-    },
-    {
-        id: 10,
-        email: 'foodiland@gmail.com',
-        subject: 'subject',
-        message: 'this is a message',
-    }
-]
+  {
+    id: 1,
+    email: "foodiland@gmail.com",
+    subject: "subject",
+    message:
+      "this is a message for :D tohid tohid tohid tohid tohid tohid tohid tohid tohid siavash siavash siavash siavash siavash siavash siavash siavash siavash siavash siavash siavash siavash siavash siavash siavash siavash siavash    ",
+  },
+  {
+    id: 2,
+    email: "foodiland@gmail.com",
+    subject: "subject",
+    message: "this is a message this is a message this is a message ",
+  },
+  {
+    id: 3,
+    email: "foodiland@gmail.com",
+    subject: "subject",
+    message:
+      "this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message this is a message ",
+  },
+  {
+    id: 4,
+    email: "foodiland@gmail.com",
+    subject: "subject",
+    message: "this is a message",
+  },
+  {
+    id: 5,
+    email: "foodiland@gmail.com",
+    subject: "subject",
+    message: "this is a message",
+  },
+  {
+    id: 6,
+    email: "foodiland@gmail.com",
+    subject: "subject",
+    message: "this is a message",
+  },
+  {
+    id: 7,
+    email: "foodiland@gmail.com",
+    subject: "subject",
+    message: "this is a message",
+  },
+  {
+    id: 8,
+    email: "foodiland@gmail.com",
+    subject: "subject",
+    message: "this is a message",
+  },
+  {
+    id: 9,
+    email: "foodiland@gmail.com",
+    subject: "subject",
+    message: "this is a message",
+  },
+  {
+    id: 10,
+    email: "foodiland@gmail.com",
+    subject: "subject",
+    message: "this is a message",
+  },
+];
 
 export default contacts;
