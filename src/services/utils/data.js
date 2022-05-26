1d7fdcaa0ba5ab672294eee693c3f233080a55de
tohid <tohidebrahimi17@gmail.com>
2022-05-26 15:41:02 +0430

    built article data

--- src/services/utils/data.js
+++ src/services/utils/data.js
@@ -1,123 +1,157 @@
 export const articleData = [
   {
     id: 1,
-    title: "Crochet Projects for Noodle Lovers ",
-    content:
-      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
-    img: "/images/delicious-recipe/food-1.png",
-    date: "12 November 2022",
-    writer: "Wade warren",
-    profile: "/images/Ellipse 3.png",
+    title: "7 Desi Sandwich Recipes For An Easy And Masaledaar Breakfast ",
+    description:
+      "We have created a list of easy sandwich recipes you can make for breakfast. But the highlight of these recipes is that they come with a desi twist ",
+    img: "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/articles%2Farticle-1.png?alt=media&token=5647bfbb-74d2-4f2b-b148-a5669eae0448",
+    created_date: "Thu May 25 2022 14:23:16 GMT+0430",
+    author: "Priyaja Bakshi",
+    profile:
+      "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/author%2Fpriyaja-bakshi.png?alt=media&token=6b202ab3-0587-413e-bd92-246e3291c813",
   },
   {
     id: 2,
-    title: "10 Vegetarian Recipes To Eat This Month",
-    content:
-      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
-    img: "/images/delicious-recipe/food-2.png",
-    date: "12 November 2022",
-    writer: "Wade warren",
-    profile: "/images/Ellipse 3.png",
+    title: "High Protein: 5 Protein-Rich Dinner Recipes",
+    description:
+      "We bring you a list of 5 protein-rich dinner recipes that you can rustle up in just 20 minutes.",
+    img: "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/articles%2Farticle-2.png?alt=media&token=886d21fa-e407-4ae2-9218-a6447bcfd90d",
+    created_date: "Thu jun 10 2022 14:23:16 GMT+0430",
+    author: "Gauri Rohatgi",
+    profile:
+      "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/author%2Fgauri-rohtagi.png?alt=media&token=3f66ee8a-8cdb-47bd-ab2b-b30680c8517c",
   },
   {
     id: 3,
-    title: "Full Guide to Becoming a Professional Chef ",
-    content:
-      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
-    img: "/images/delicious-recipe/food-3.png",
-    date: "12 November 2022",
-    writer: "Wade warren",
-    profile: "/images/Ellipse 3.png",
+    title: "Mango Pickle, Onion Pickle And More: 5 Instant Pickle Recipes ",
+    description:
+      "Here we bring you a list of pickle recipes that you can make in a matter of minutes. They need just 15-20 minutes to get ready. So, what are you waiting for? Let's get started with the list ",
+    img: "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/articles%2Farticle-3.png?alt=media&token=637474fc-d907-4e64-b95e-82debdcce816",
+    created_date: "Thu feb 29 2022 14:23:16 GMT+0430",
+    author: "Gauri Rohatgi",
+    profile:
+      "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/author%2Fgauri-rohtagi.png?alt=media&token=3f66ee8a-8cdb-47bd-ab2b-b30680c8517c",
   },
   {
     id: 4,
-    title: "Simple & Delicious Vegetarian Lasagna ",
-    content:
-      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
-    img: "/images/delicious-recipe/food-4.png",
-    date: "12 November 2022",
-    writer: "Wade warren",
-    profile: "/images/Ellipse 3.png",
+    title: "Indian Cooking Tips: 5 Desi Egg-Based Snack Recipes",
+    description:
+      "Looking for unique party snack recipes? We have found five egg-based delicious snacks that have an Indian/desi tadka",
+    img: "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/articles%2Farticle-4.png?alt=media&token=d8c0f91b-20e7-4035-b0ec-113ebabbd402",
+    created_date: "Thu oct 05 2022 14:23:16 GMT+0430",
+    author: "Priyaja Bakshi",
+    profile:
+      "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/author%2Fpriyaja-bakshi.png?alt=media&token=6b202ab3-0587-413e-bd92-246e3291c813",
   },
   {
     id: 5,
-    title: "Plantain and Pinto Stew with Aji Verde ",
-    content:
-      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
-    img: "/images/delicious-recipe/food-5.png",
-    date: "12 November 2022",
-    writer: "Wade warren",
-    profile: "/images/Ellipse 3.png",
+    title: "Weight Loss: 5 Delicious Low Cal Egg Salad Recipes",
+    description:
+      "Struggling to lose those extra kilos? We have some amazing recipes for you. These eggs salad recipes are not only healthy but also delicious!",
+    img: "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/articles%2Farticle-5.png?alt=media&token=e5334118-36d0-48c8-8900-51fdbd512c9c",
+    created_date: "Thu May 25 2022 14:23:16 GMT+0430",
+    author: "Priyaja Bakshi",
+    profile:
+      "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/author%2Fpriyaja-bakshi.png?alt=media&token=6b202ab3-0587-413e-bd92-246e3291c813",
   },
   {
     id: 6,
-    title: "We’re Hiring a Communications Assistant! ",
-    content:
-      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
-    img: "/images/delicious-recipe/food-6.png",
-    date: "12 November 2022",
-    writer: "Wade warren",
-    profile: "/images/Ellipse 3.png",
+    title: "Healthy Lunchbox Recipes: Delicious Dishes  ",
+    description:
+      "Lunchbox Recipes: With colleges and offices opening up rapidly, you might be in the search of delicious and interesting lunch box ideas. Here are 5 easy, healthy, and quick recipes that you can pack in the coming days.",
+    img: "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/articles%2Farticle-6.png?alt=media&token=b30be48d-c5d8-47a2-8481-6d695bb64205",
+    created_date: "Thu jul 25 2022 14:23:16 GMT+0430",
+    author: "Pragya Subedy",
+    profile:
+      "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/author%2Fpragya-subedy.png?alt=media&token=63fce17d-8adb-4f5a-817b-3503b2033354",
   },
   {
     id: 7,
-    title: "Big and Juicy Wagyu Beef ",
-    content:
-      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
-    img: "/images/delicious-recipe/food-7.png",
-    date: "12 November 2022",
-    writer: "Wade warren",
-    profile: "/images/Ellipse 3.png",
+    title: "Watch: How To Make Chocolate Sauce In 5 Mins ",
+    description:
+      "We found a super easy chocolate sauce recipe that is not only fuss-free but also takes just five minutes for preparation.   ",
+    img: "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/articles%2Farticle-7.png?alt=media&token=64e85af9-d85b-4470-ae3b-ab6518396272",
+    created_date: "Thu May 25 2022 14:23:16 GMT+0430",
+    author: "Somdatta Saha",
+    profile:
+      "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/author%2Fsomdatta-saha.png?alt=media&token=6c4439d0-dadb-4d68-893b-3c1ffea8b650",
   },
   {
     id: 8,
-    title: "Big and Juicy Wagyu Beef ",
-    content:
-      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
-    img: "/images/delicious-recipe/food-8.png",
-    date: "12 November 2022",
-    writer: "Wade warren",
-    profile: "/images/Ellipse 3.png",
+    title: "How To Make Egg And Potato Salad For A Quick Meal  ",
+    description:
+      "This egg-potato salad recipe is light, wholesome and takes less than 30 minutes for preparation. Find the recipe inside.",
+    img: "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/articles%2Farticle-8.png?alt=media&token=79c1d7ec-f5b3-4558-8a2c-9871ca86339b",
+    created_date: "Thu may 21 2022 14:23:16 GMT+0430",
+    author: "Somdatta Saha",
+    profile:
+      "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/author%2Fsomdatta-saha.png?alt=media&token=6c4439d0-dadb-4d68-893b-3c1ffea8b650",
   },
   {
     id: 9,
-    title: "Big and Juicy Wagyu Beef ",
-    content:
-      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
-    img: "/images/delicious-recipe/food-1.png",
-    date: "12 November 2022",
-    writer: "Wade warren",
-    profile: "/images/Ellipse 3.png",
+    title: "5 Things You Can Use Instead Of Breadcrumbs To Coat Fried Foods",
+    description:
+      "Ran out of breadcrumbs? Use these 5 food items which work the same for coating fried foods and snacks.",
+    img: "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/articles%2Farticle-9.png?alt=media&token=05918412-d698-45e1-a26c-87c0e5c39366",
+    created_date: "Thu feb 11 2022 14:23:16 GMT+0430",
+    author: "Pragya Subedy",
+    profile:
+      "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/author%2Fpragya-subedy.png?alt=media&token=63fce17d-8adb-4f5a-817b-3503b2033354",
   },
   {
     id: 10,
-    title: "Big and Juicy Wagyu Beef ",
-    content:
-      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
-    img: "/images/delicious-recipe/food-2.png",
-    date: "12 November 2022",
-    writer: "Wade warren",
-    profile: "/images/Ellipse 3.png",
+    title: "How To Make Garlic Bread Without Using An Oven ",
+    description:
+      "If you love garlic bread as much as we do, here we have something special for you. We have found one recipe for making restaurant-style garlic bread at home and that too without an oven.",
+    img: "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/articles%2Farticle-10.png?alt=media&token=3c83edb2-1ab2-43c3-97f7-526091c6f228",
+    created_date: "Thu May 25 2022 14:23:16 GMT+0430",
+    author: "Gauri Rohatgi",
+    profile:
+      "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/author%2Fgauri-rohtagi.png?alt=media&token=3f66ee8a-8cdb-47bd-ab2b-b30680c8517c",
   },
   {
     id: 11,
-    title: "Big and Juicy Wagyu Beef ",
-    content:
-      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
-    img: "/images/delicious-recipe/food-3.png",
-    date: "12 November 2022",
-    writer: "Wade warren",
-    profile: "/images/Ellipse 3.png",
+    title: "Afghani Tikka, Pulao And More: 7 Afghani Recipes  ",
+    description:
+      "If you are a true foodie and love to try and experiment with food, you are at the right place. Here we bring you a list of 7 Afghani recipes you can try making at home.",
+    img: "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/articles%2Farticle-11.png?alt=media&token=ed4bb538-97f8-4e18-9d83-9a7100aa8b0d",
+    created_date: "Thu jun 26 2022 14:23:16 GMT+0430",
+    author: "Gauri Rohatgi",
+    profile:
+      "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/author%2Fgauri-rohtagi.png?alt=media&token=3f66ee8a-8cdb-47bd-ab2b-b30680c8517c",
   },
   {
     id: 12,
-    title: "Big and Juicy Wagyu Beef ",
-    content:
-      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
-    img: "/images/delicious-recipe/food-4.png",
-    date: "12 November 2022",
-    writer: "Wade warren",
-    profile: "/images/Ellipse 3.png",
+    title: "5 Crispy Non-Vegetarian Snacks To Indulge On A Rainy Day",
+    description:
+      "We have found the recipes for crispy non-vegetarian snacks that shall soothe our rainy-day desires for something crunchy, meaty and delicious. ",
+    img: "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/articles%2Farticle-12.png?alt=media&token=7366b281-d728-4f5a-bb44-3a64bec25dad",
+    created_date: "Thu nov 25 2022 14:23:16 GMT+0430",
+    author: "Priyaja Bakshi",
+    profile:
+      "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/author%2Fpriyaja-bakshi.png?alt=media&token=6b202ab3-0587-413e-bd92-246e3291c813",
+  },
+  {
+    id: 13,
+    title: "A Lip-Smacking Recipe That Will Fire Up Your Meals",
+    description:
+      "The onion chutney is all things flavourful and has a taste that you have never had before. It is comforting, spicy, and makes for a perfect side dish ",
+    img: "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/articles%2Farticle-13.png?alt=media&token=8bb51a5e-e6d9-42eb-8c7d-6b8bce78f306",
+    created_date: "Thu May 25 2022 14:23:16 GMT+0430",
+    author: "Somdatta Saha",
+    profile:
+      "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/author%2Fsomdatta-saha.png?alt=media&token=6c4439d0-dadb-4d68-893b-3c1ffea8b650",
+  },
+  {
+    id: 14,
+    title: "Summer Special: Mango Sandwich Is The Recent Trend",
+    description:
+      "Made with just about 2-3 major ingredients, the mango sandwich is another fun way to include mango in your diet. Use chilled ingredients for the best experience!",
+    img: "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/articles%2Farticle-14.png?alt=media&token=a4a63446-9083-4c25-be3c-c8c2eb3e2ec8",
+    created_date: "Thu May 25 2022 14:23:16 GMT+0430",
+    author: "Pragya Subedy",
+    profile:
+      "https://firebasestorage.googleapis.com/v0/b/foodieland-3b1ed.appspot.com/o/author%2Fpragya-subedy.png?alt=media&token=63fce17d-8adb-4f5a-817b-3503b2033354",
   },
 ];
 
