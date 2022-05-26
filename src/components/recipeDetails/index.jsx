a4003b396f4acb868a4b547b796ac753d866d861
tohid <tohidebrahimi17@gmail.com>
2022-05-26 19:58:31 +0430

    refactored recipe details page fetching

--- src/components/recipeDetails/index.jsx
+++ src/components/recipeDetails/index.jsx
@@ -1,6 +1,5 @@
 import RecipeVideoCard from "../recipeVideoCard";
 import NutritionInfoCard from "./components/nutritionInfoCard";
-import React, { useEffect, useState } from "react";
 import { icons } from "../../services/utils/icons";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -19,52 +18,28 @@ let month = [
   "Nov",
   "Dec",
 ];
-const initialRecipe = {
-  title: "",
-  video: "",
-  description: "",
-  cookTime: "",
-  prepTime: "",
-  nutrition: {
-    calories: "",
-    carbohydrate: "",
-    cholesterol: "",
-    protein: "",
-    total_fat: "",
-  },
-  chef: "",
-  chefImg: "",
-  category: "",
-  uploadDate: "",
-};
 function RecipeDetails() {
-  const [recipe, setRecipe] = useState(initialRecipe);
   const { recipes } = useSelector((state) => state.recipes);
   const { timer, fork, knife, printer, share } = icons;
   const { id } = useParams();
 
-  const date = new Date(recipe.uploadDate);
+  const {
+    title,
+    video,
+    description,
+    cook_time: cookTime,
+    prep_time: prepTime,
+    nutrition,
+    chef,
+    uploadDate,
+    category,
+    chefImg,
+  } = recipes.filter((item) => item.id === id)[0];
+
+  const date = new Date(uploadDate);
   const dateString = `${date.getDay()} ${
     month[date.getMonth()]
   } ${date.getFullYear()}`;
-
-  useEffect(() => {
-    if (recipes.length > 0) {
-      const [selectedRecipe] = recipes.filter((r) => r.id === id);
-      setRecipe({
-        title: selectedRecipe.title,
-        video: selectedRecipe.video,
-        description: selectedRecipe.description,
-        cookTime: selectedRecipe.cook_time,
-        prepTime: selectedRecipe.prep_time,
-        nutrition: selectedRecipe.nutrition,
-        chef: selectedRecipe.chef,
-        chefImg: selectedRecipe.chefImg,
-        category: selectedRecipe.category,
-        uploadDate: selectedRecipe.uploadDate,
-      });
-    }
-  }, [recipes]);
 
   return (
     <section>
@@ -72,21 +47,21 @@ function RecipeDetails() {
         <div className="basis-full   lg:basis-[66%] ">
           <div className="text-center lg:text-left">
             <h1 className="font-semibold text-3xl sm:text-4xl md:text-5xl xl:text-6xl ">
-              {recipe.title}
+              {title}
             </h1>
           </div>
           <div className="flex justify-end lg:justify-start flex-wrap gap-6 sm:gap-2 md:gap-4 my-12">
             <div className="basis-[44%] sm:basis-[23%] flex border-r border-gray-300 border-solid ">
               <div>
                 <img
-                  src={`${recipe.chefImg}`}
+                  src={`${chefImg}`}
                   alt="chief"
                   className="mx-auto w-10 h-10 object-cover rounded-[50%]"
                 />
               </div>
               <div className=" ml-2 ">
                 <span className="font-bold block text-xs md:text-sm xl:text-base">
-                  {recipe.chef}
+                  {chef}
                 </span>
                 <span className="carousel-gray-text-color text-2xs xl:text-xs  font-medium">
                   {dateString}
@@ -98,7 +73,7 @@ function RecipeDetails() {
               <div className="ml-2">
                 <span className="font-medium text-xs block">PREP TIME</span>
                 <span className="text-sm font-medium text-secondary ">
-                  {recipe.prepTime}
+                  {prepTime}
                 </span>
               </div>
             </div>
@@ -107,17 +82,17 @@ function RecipeDetails() {
               <div className="ml-2">
                 <span className="font-medium text-xs block">COOK TIME</span>
                 <span className="text-sm font-medium text-secondary ">
-                  {recipe.cookTime}
+                  {cookTime}
                 </span>
               </div>
             </div>
             <div className="basis-[44%] sm:basis-[23%] flex items-center text-sm md:text-base">
               <span className="inline-block ">{fork()}</span>
               <span className="inline-block pr-2">{knife()}</span>
-              {recipe.category}
+              {category}
             </div>
           </div>
-          <RecipeVideoCard videoUrl={recipe.video} />
+          <RecipeVideoCard videoUrl={video} />
         </div>
         <div className="basis-full lg:basis-[30%] flex flex-col justify-between">
           <div className="flex justify-center lg:justify-end py-8">
@@ -134,10 +109,10 @@ function RecipeDetails() {
               <span className="text-xs font-medium">SHARE</span>
             </div>
           </div>
-          <NutritionInfoCard nutrition={recipe.nutrition} />
+          <NutritionInfoCard nutrition={nutrition} />
         </div>
         <div className="mt-10">
-          <p className="text-secondary text-base">{recipe.description}</p>
+          <p className="text-secondary text-base">{description}</p>
         </div>
       </div>
     </section>
